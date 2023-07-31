0f810cba4ae4972aa02ac7cd4093afb623ae7cd9
SuluDju <107399650+SuluDju@users.noreply.github.com>
2023-07-31 22:14:17 +0600

    Add files via upload

---
+++ precache-manifest.9d2352aeb4edbf4ec3da74bb13ff2efb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0617b94a60f9d34ebecef3bc113158b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc428420d7c4bd4053a6",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "f8cda03a0698feb3be5b",
+    "url": "/static/js/2.612d2c84.chunk.js"
+  },
+  {
+    "revision": "cc428420d7c4bd4053a6",
+    "url": "/static/js/main.75375d11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
